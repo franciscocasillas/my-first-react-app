94997dc766bde835541b9984c136271fb0426824
Francisco J. Casillas <chiccasillas@gmail.com>
2024-04-23 12:42:46 -0600

    Assign props to Testimonio component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ function App() {
 		<div className="App">
 			<div className="contenedor-principal">
 				<h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-				<Testimonio />
+				<Testimonio
+					nombre="Shawn Wang"
+					pais="Singapur"
+					imagen="Shawn"
+					cargo="Ingeniero de Software"
+					empresa="Amazon"
+					testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+				/>
 			</div>
 		</div>
 	);
